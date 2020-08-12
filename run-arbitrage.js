3c026b191084d84a42efd58f3c6cff107350e29a
Vinzalpha <blogvinzalpha@gmail.com>
2020-08-12 09:55:07 +0200

    create smart contract Flashloans

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -4,10 +4,12 @@ const Web3 = require('web3');
 const { ChainId, Token, TokenAmount, Pair } = require('@uniswap/sdk');
 const abis = require('./abis');
 const { mainnet: addresses } = require('./addresses');
+const Flashloan = require('./build/contracts/Flashloan.json');
 
 const web3 = new Web3(
     new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
 );
+const { address: admin } = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY);
 
 const kyber = new web3.eth.Contract(
     abis.kyber.kyberNetworkProxy,
@@ -18,8 +20,18 @@ const AMOUNT_ETH = 100;
 const RECENT_ETH_PRICE = 380; // in USD
 const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
 const AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_ETH_PRICE).toString());
+const DIRECTION = {
+    KYBER_TO_UNISWAP: 0,
+    UNISWAP_TO_KYBER: 1
+};
 
 const init = async () => {
+    const networkId = await WebGL2RenderingContext.eth.net.getId();
+    const flashloan = new WebGL2RenderingContext.eth.Contract(
+        Flashloan.abi,
+        Flashloan.networks[networkId].address
+    );
+
     const [dai, weth] = await Promise.all(
         [addresses.tokens.dai, addresses.tokens.weth].map(tokenAddress => (
             Token.fetchData(
@@ -29,7 +41,7 @@ const init = async () => {
         )));
     const daiWeth = await Pair.fetchData(
         dai,
-        weth
+        weth,
     );
     
 
@@ -50,8 +62,8 @@ const init = async () => {
                 kyber
                     .methods
                     .getExpectedRate(
-                        addresses.tokens.dai,
                         '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+                        addresses.tokens.dai,
                         AMOUNT_ETH_WEI
                     )
                     .call(), 
@@ -60,7 +72,7 @@ const init = async () => {
 
             const kyberRates = {
                 buy: parseFloat(1 / (kyberResults[0].expectedRate / ( 10 ** 18))),
-                sell: parseFloat(1 / (kyberResults[1].expectedRate / ( 10 ** 18 ))),
+                sell: parseFloat(kyberResults[1].expectedRate / (10 ** 18)),
             };
             console.log('Kyber ETH/DAI');
             console.log(kyberRates);
@@ -74,17 +86,29 @@ const init = async () => {
             console.log("uniswapResult", uniswapResults[1][0].toExact())
             
             const uniswapRates = {
-                buy: parseFloat((AMOUNT_DAI_WEI / uniswapResults[0][0].toExact()) / (10 ** 18)),
+                buy: parseFloat( AMOUNT_DAI_WEI / (uniswapResults[0][0].toExact() * 10 ** 18)),
                 sell: parseFloat((uniswapResults[1][0].toExact() / AMOUNT_ETH)),
             }
             console.log('Uniswap ETH/DAI');
             console.log(uniswapRates);
-            
-            const gasPrice = await web3.eth.getGasPrice();
-            const txCost = 200000 * parseInt(gasPrice);
+
+            const [tx1, tx2] = Object.keyx(DIRECTION).map(direction => flashloan.methods.initiateFlashloan(
+                addresses.dydx.solo,
+                addresses.tokens.dai,
+                AMOUNT_DAI_WEI,
+                DIRECTION[direction]
+            ));
+            const [gasPrice, gasCost1, gasCost2] = await Promise.all([
+                web3.eth.getGasPrice(),
+                tx1.estimateGas({from: admin }),
+                tx2.estimateGas({from: admin }),
+            ]);
+
+            const txCost1 = parseInt(gasCost1) * parseInt(gasPrice);
+            const txCost2 = parseInt(gasCost2) * parseInt(gasPrice);
             const currentEthPrice = (uniswapRates.buy + uniswapRates.sell) / 2;
-            const profit1 = (parseInt(AMOUNT_ETH_WEI) / (10 ** 18)) * ((uniswapRates.sell - kyberRates.buy) - (txCost / (10 ** 18)) * currentEthPrice);
-            const profit2 = (parseInt(AMOUNT_ETH_WEI) / (10 ** 18)) * ((kyberRates.sell - uniswapRates.buy) - (txCost / (10 ** 18)) * currentEthPrice);
+            const profit1 = (parseInt(AMOUNT_ETH_WEI) / (10 ** 18)) * ((uniswapRates.sell - kyberRates.buy) - (txCost1 / (10 ** 18)) * currentEthPrice);
+            const profit2 = (parseInt(AMOUNT_ETH_WEI) / (10 ** 18)) * ((kyberRates.sell - uniswapRates.buy) - (txCost2 / (10 ** 18)) * currentEthPrice);
 
             console.log(parseInt(AMOUNT_ETH_WEI  / (10 ** 18)))
             console.log(currentEthPrice * (txCost  / (10 ** 18)))
@@ -98,17 +122,37 @@ const init = async () => {
                 console.log(`Buy Eth on Kyber at ${kyberRates.buy} dai`);
                 console.log(`Sell Eth on Uniswap at ${uniswapRates.sell} dai`);
                 console.log(`Expected profit: ${profit1} dai`);
+                const data = tx1.encodeABI();
+                const txData = {
+                    from: admin,
+                    to: flashloan.options.address,
+                    data,
+                    gas: gasCost1,
+                    gasPrice
+                };
+                const receipt = await web3.eth.sendTransaction(txData);
+                console.log(`Transaction hash: ${receipt.transactionHash}`)
             }
-            if(profit2 > 0) {
+            else if(profit2 > 0) {
                 console.log('An opportunity found!');
                 console.log(`Buy Eth on Uniswap at ${uniswapRates.buy} dai`);
                 console.log(`Sell Eth on Kyber at ${kyberRates.sell} dai`);
                 console.log(`Expected profit: ${profit2} dai`);
+                const data = tx2.encodeABI();
+                const txData = {
+                    from: admin,
+                    to: flashloan.options.address,
+                    data,
+                    gas: gasCost2,
+                    gasPrice
+                };
+                const receipt = await web3.eth.sendTransaction(txData);
+                console.log(`Transaction hash: ${receipt.transactionHash}`)
             }
         })
 
         .on('error', error => {
             console.log(error);
-        })
+        });
 }
 init();
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -58,16 +60,13 @@ module.exports = {
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    mainnet: {
+      provider: () => new HDWalletProvider(
+        process.env.PRIVATE_KEY,
+        process.env.INFURA_URL
+      ),
+      network_id: 1
+    }
 
     // Useful for private networks
     // private: {
